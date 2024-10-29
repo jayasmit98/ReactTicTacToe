afcf4d789234db16a74d4a8f7fe7f80bf3e690a2
jayasmit98 <jayasmit98@gmail.com>
2024-10-29 18:56:28 +0530

    added history functionality

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ function calcuateSquares(squares){
   return null;
 }
 
-function Board() { 
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setxIsNext] = useState(true);
+function Board({xIsNext, squares, onPlay}) { 
+  // const [squares, setSquares] = useState(Array(9).fill(null))
+  // const [xIsNext, setxIsNext] = useState(true);
   function handleClick(i){
     if(squares[i] || calcuateSquares(squares)){
       return;
@@ -42,9 +42,7 @@ function Board() {
     }else{
       squareArray[i] = 'O';
     }
-    setxIsNext(!xIsNext);
-
-    setSquares(squareArray);
+    onPlay(squareArray);
   }
 
   const winner = calcuateSquares(squares);
@@ -78,4 +76,50 @@ function Board() {
   )  
 }
 
-export default Board;
+export default function Game(){
+  const [xIsNext, setxIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]); 
+  const [currentMove, setCurrentMove] =  useState(0);
+  const currentSquares = history[currentMove];
+
+
+
+  function handlePlay(nextSquares){
+    //TODO
+    const nextHistory = [...history.slice(0, currentMove +1),nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setxIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    setxIsNext(nextMove % 2 == 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move>0){
+      description = 'Go to move #' + move;
+    }else{
+      description = 'Go to game start'
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className = "game">
+      <div className = "game-board">
+        <Board xIsNext={xIsNext}  squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+};
